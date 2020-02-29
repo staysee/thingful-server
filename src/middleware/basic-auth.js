7dfcace4b8245504353d3673e049b85216e519ef
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-02-28 19:01:40 -0800

    saving

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -6,7 +6,7 @@ function requireAuth(req, res, next) {
     const authToken = req.get('Authorization') || ''
 
     let basicToken
-    if (!authToken.toLowerCase().startsWith('basic')){
+    if (!authToken.toLowerCase().startsWith('basic ')){
         return res.status(401).json({ error: 'Missing basic token' })
     } else {
         basicToken = authToken.slice('basic '.length, authToken.length)
